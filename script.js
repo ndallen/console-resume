9baf12ef7fef9ef5275e82e5139f4b60571b88d5
ndallen <ecommerceevolution@gmail.com>
2019-03-09 15:56:57 +0800

    console resume functions added.Completed exercise

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const resume = {
   name: 'Neil Allen',
   career: 'Entrepreneur / Web Developer',
-  description: 'I live in beatiful Thailand with my wife, son and labrador Rosie. I am into all things tech and currently self learning front end Javascript.',
+  description: 'I live in beautiful Thailand with my wife, son and labrador Rosie. I am into all things tech and currently self learning front end Javascript.',
   interests: ['Football', 'Music', 'Playing guitar', 'Coding', 'Gaming', 'Movies', 'Food'],
   previousExperience: [
     'started ecommerce retail company that turned over $2M revenue in 4 years',
@@ -11,3 +11,43 @@ const resume = {
   skills:['HTML', 'CSS', 'Bootstrap', 'BAM: Javascript', 'BAM: Photoshop', 'BAM: Guitar']
 }
 
+//name (CAPITALISED), job title and description 
+
+function displayPosition(name, career, description){
+console.log(`Name: ${name.toUpperCase()}`);
+console.log(`Career: ${career}`)
+console.log(`Description: ${description}`)
+
+}
+displayPosition(resume.name,resume.career, resume.description);
+
+function displayInterests(previousExperience) {
+  console.log('My Interests includes;')
+  for (let i = 0; i < previousExperience.length; i++) {
+
+    console.log('* ' + previousExperience[i]);
+  }
+}
+
+displayInterests(resume.interests)
+
+function displayExperience(previousExperience) {
+  console.log('My Previous Experience includes;')
+  for(let i = 0; i < previousExperience.length; i++) {
+    
+    console.log('* ' + previousExperience[i]);
+  }
+}
+
+displayExperience(resume.previousExperience);
+
+
+function displaySkill(){
+  console.log('My Skills Include;');
+  for(i = 0; i < resume.skills.length; i++) {
+    console.log('* ' + resume.skills[i]);
+  }
+  
+};
+
+displaySkill();
