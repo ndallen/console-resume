01805aa3ef05601ab0b99ae3e637a07dad104c6b
ndallen <ecommerceevolution@gmail.com>
2019-03-09 15:07:43 +0800

    added resume object

---
+++ script.js
@@ -0,0 +1,13 @@
+const resume = {
+  name: 'Neil Allen',
+  career: 'Entrepreneur / Web Developer',
+  description: 'I live in beatiful Thailand with my wife, son and labrador Rosie. I am into all things tech and currently self learning front end Javascript.',
+  interests: ['Football', 'Music', 'Playing guitar', 'Coding', 'Gaming', 'Movies', 'Food'],
+  previousExperience: [
+    'started ecommerce retail company that turned over $2M revenue in 4 years',
+    '15 years experience of ecommerce marketplaces such as Amazon, Ebay, Fruugo',
+    'In a previous life I was a production planner with SAP skills'
+  ],
+  skills:['HTML', 'CSS', 'Bootstrap', 'BAM: Javascript', 'BAM: Photoshop', 'BAM: Guitar']
+}
+
